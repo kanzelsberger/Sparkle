1aa5dd0226d4375ddbb333c17bd87b624fa69959
Pavel Kanzelsberger <pavel@kanzelsberger.com>
2023-08-02 17:13:52 +0200

    Release 2.4.0-fork-v1 (#11)
    * Handle invalid % character at the end of EdDSA public key in Info.plist
    
    * Add decoding log
    
    * Fix logging
    
    * MaybeFinalFix ... maybe
    
    * Improve logging
    
    * Remove binary target from swift package
    
    * Add path to swift package
    
    * Binary release 2.4.0-fork-v1

--- Package.swift
+++ Package.swift
@@ -1,6 +1,12 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+// Version is technically not required here, SPM doesn't check
+let version = "2.4.0-fork-v1"
+// Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
+let tag = "2.4.0-fork-v1"
+let checksum = "f254f76c5bc8438c4ceef2def259a7fcb664dc1ae821ead08358d5702289b324"
+let url = "https://github.com/kanzelsberger/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
     name: "Sparkle",
@@ -11,10 +17,10 @@ let package = Package(
             targets: ["Sparkle"])
     ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "Sparkle",
-            path: "Sparkle",
-            dependencies: []
+            url: url,
+            checksum: checksum
         )
     ]
 )
