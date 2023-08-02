a0728ab3555c4e0ec483f12966706cfd59e6c24f
Pavel Kanzelsberger <pavel@kanzelsberger.com>
2023-08-02 16:16:41 +0200

    Fix swift package (#9)
    * Handle invalid % character at the end of EdDSA public key in Info.plist
    
    * Add decoding log
    
    * Fix logging
    
    * MaybeFinalFix ... maybe
    
    * Improve logging
    
    * Remove binary target from swift package

--- Package.swift
+++ Package.swift
@@ -1,12 +1,6 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-// Version is technically not required here, SPM doesn't check
-let version = "2.5.0-beta.1"
-// Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
-let tag = "2.5.0-beta.1"
-let checksum = "cfd39886db2d1c9b84e23e5127fad0a41f216e40af97fa7ff063feb0211b6e61"
-let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
     name: "Sparkle",
@@ -17,10 +11,9 @@ let package = Package(
             targets: ["Sparkle"])
     ],
     targets: [
-        .binaryTarget(
+        .target(
             name: "Sparkle",
-            url: url,
-            checksum: checksum
+            dependencies: []
         )
     ]
 )
